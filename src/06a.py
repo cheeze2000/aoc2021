3876f299f16cc05a2ea1dd2f1f5d24df0a6e7908
cheeze2000 <me@cheeze2000.net>
2021-12-07 13:54:27 +0800

    refactor: rewrite day 06 to py

---
+++ src/06a.py
@@ -0,0 +1,17 @@
+def f(xs):
+	ys = [0] * 9
+	for i in range(9):
+		if i > 0:
+			ys[i - 1] += xs[i]
+		else:
+			ys[6] += xs[0]
+			ys[8] += xs[0]
+
+	return ys
+
+xs = [int(i) for i in input().split(",")]
+ys = [0] * 9
+for x in xs: ys[x] += 1
+
+for i in range(80): ys = f(ys)
+print(sum(ys))
